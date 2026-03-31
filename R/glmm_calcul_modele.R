58f2594c59ce0b1460352c69fe7db5d7e1c54dd3
VIGNERON Pablo <pablo.vigneron@ofb.gouv.fr>
2026-03-31 17:46:38 +0200

    final draft of the species selection script

---
+++ R/glmm_calcul_modele.R
@@ -0,0 +1,43 @@
+glmm_calcul_modele <- function(data,
+                               mon_espece) {
+  
+  filtered_data <- data %>%
+    filter(espece == mon_espece)
+  
+  if (nrow(filtered_data) < 2 || 
+      length(unique(filtered_data$pop_id)) < 2 || 
+      length(unique(filtered_data$annee)) < 2 || 
+      length(unique(filtered_data$pro_libelle)) < 2 ) {
+    return(NULL)
+  }
+  
+
+  model <- try(glmer(valeur ~ scale(annee) +
+                       (1 | pop_id) +
+                       scale(annee) +
+                       pro_libelle +
+                       scale(ope_surface_calculee) +
+                       scale(julian) +
+                       scale(I(julian^2)),
+                     data = filtered_data,
+                     family = poisson),
+               silent = TRUE)
+
+  if (inherits(model, "try-error")) {
+    return(NULL)
+  }
+  
+  res <- summary(model)$coefficients %>%
+    as.data.frame() %>%
+    rename(p_value = `Pr(>|z|)`) %>%
+    mutate(sig = case_when(
+      p_value < 0.001 ~ "***",
+      p_value < 0.01 ~ "**",
+      p_value < 0.05 ~ "*",
+      TRUE ~ ""
+    )) %>%
+    mutate(esp_code_alternatif = mon_espece)
+  
+  return(res)
+}
+
