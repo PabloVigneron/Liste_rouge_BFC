7b9c5efdbbdf129f46866356551814c30c7f362e
VIGNERON Pablo <pablo.vigneron@ofb.gouv.fr>
2026-04-08 18:27:54 +0200

    Create function for GAMMs and GLMM and first graph

--- R/gamm_calcul_modele.R
+++ R/gamm_calcul_modele.R
@@ -1,18 +1,19 @@
-#' Calculer les résultats des modèles GLMM pour un df et une espèce choisit
+#' Calculer les résultats des modèles GAMMs pour un df et une espèce choisit
 #'
 #' @param df Dataframe contenant les données
 #' @param mon_espece esp_code_alternatif : le code espèce de l'espèce ciblé par l'analyse
 #' @return Dataframe avec resultats 
 #' @export 
 #' 
-#' @importClassesFrom dplyr; mutate filter rename ;lme4, glmer; base summary  nrow rename length unique 
+#' @importClassesFrom dplyr; mutate filter rename ; mgcv, ; base summary  nrow rename length unique 
 #'
 #' @examples
 #' \dontrun{
-#' mod <- glmm_calcul_modele (df = esp_ope_selection, mon_espece == "BOU")
+#' mod <- gamm_calcul_modele (df = esp_ope_selection, mon_espece == "BOU")
 #' }
 
-glmm_calcul_modele <- function(data, mon_espece) {
+
+gamm_calcul_modele <- function(data, mon_espece) {
   filtered_data <- data %>%
     filter(espece == mon_espece)
   
@@ -24,13 +25,15 @@ glmm_calcul_modele <- function(data, mon_espece) {
   }
   
   
-  model <- try(glmer(
-    valeur ~ annee +
-      ope_surface_calculee +
-      pro_libelle +
-      (1 | pop_id) +
-      data = filtered_data,
+  model <- try(gam(
+    valeur ~ s(annee),
+    +ope_surface_calculee,
+    +pro_libelle,
+    +s(price),
+    +(1 |pop_id),
+    data = filtered_data,
     family = poisson(link = "log"),
+    method = "REML"
   ),
   silent = TRUE)
   
